989c4768da764c772ca1ac794b780a7cf10211b5
Brenel Galicia <jesus.galicia.23e@utzmg.edu.mx>
2025-11-22 10:45:03 -0600

    feat: add billing registration API route

--- routes/facturacion.js
+++ routes/facturacion.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+
 const FacturacionController = require('../controllers/FacturacionController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-router.post('/registrar', authMiddleware, FacturacionController.registrarFacturacion);
+router.post('/registrar', authMiddleware, FacturacionController.actualizarFacturacion);
 
 module.exports = router;
